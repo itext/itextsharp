3f5e90abbdd4ca74cb1aa32280e22e1756fc51fb
Paulo Soares <psoares@gmail.com>
2009-07-08 15:28:35 +0000

    SNAPSHOT next release.

--- src/core/iTextSharp/text/Document.cs
+++ src/core/iTextSharp/text/Document.cs
@@ -109,7 +109,7 @@ namespace iTextSharp.text {
         * This constant may only be changed by Paulo Soares and/or Bruno Lowagie.
         * @since	2.1.6
         */
-	    private const String RELEASE = "4.1.6";
+	    private const String RELEASE = "5.0.0_SNAPSHOT";
 	    /** This constant may only be changed by Paulo Soares and/or Bruno Lowagie. */
 	    private const String ITEXT_VERSION = ITEXT + " " + RELEASE + " by 1T3XT";
 
