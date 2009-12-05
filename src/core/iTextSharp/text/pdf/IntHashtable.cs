facf3c15c5330bbb6dcfab8b09b2cc53f9766c08
Paulo Soares <psoares@gmail.com>
2009-12-05 19:28:58 +0000

    Change of license to AGPL and removal of a mail address.

--- src/core/iTextSharp/text/pdf/IntHashtable.cs
+++ src/core/iTextSharp/text/pdf/IntHashtable.cs
@@ -1,12 +1,34 @@
 using System;
 using iTextSharp.text.error_messages;
 
-// IntHashtable - a Hashtable that uses ints as the keys
-//
-// This is 90% based on JavaSoft's java.util.Hashtable.
-//
-// Visit the ACME Labs Java page for up-to-date versions of this and other
-// fine Java utilities: http://www.acme.com/java/
+/*
+ * This class is based on org.apache.IntHashMap.commons.lang
+ * http://jakarta.apache.org/commons/lang/xref/org/apache/commons/lang/IntHashMap.html
+ * It was adapted by Bruno Lowagie for use in iText,
+ * reusing methods that were written by Paulo Soares.
+ * Instead of being a hashtable that stores objects with an int as key,
+ * it stores int values with an int as key.
+ * 
+ * This is the original license of the original class IntHashMap:
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Note: originally released under the GNU LGPL v2.1, 
+ * but rereleased by the original author under the ASF license (above).
+ */
 
 namespace iTextSharp.text.pdf {
 
