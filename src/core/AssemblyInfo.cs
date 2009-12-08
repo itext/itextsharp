07a77bfade9ea21702f44c14579d11622e9906f8
Paulo Soares <psoares@gmail.com>
2009-12-08 10:03:31 +0000

    Release 5.0.0

--- src/core/AssemblyInfo.cs
+++ src/core/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.1.6")]
+[assembly: AssemblyVersion("5.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
