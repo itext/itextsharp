2247efb1d6b49e6e4a3afa456ffd9f09f8e52d4c
Paulo Soares <psoares@gmail.com>
2012-03-31 15:51:10 +0000

    Corrected assembly version of xmlworker to 1.1.2.

--- src/extras/itextsharp.xmlworker/Properties/AssemblyInfo.cs
+++ src/extras/itextsharp.xmlworker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("itextsharp.xmlworker")]
-[assembly: AssemblyCopyright("Copyright (C) 2011 by Bruno Lowagie and Paulo Soares. All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2012 by Bruno Lowagie and Paulo Soares. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
