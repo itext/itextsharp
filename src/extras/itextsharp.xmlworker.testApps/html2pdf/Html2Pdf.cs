7271b7b4541ebd708f6ca12905490ce7235f2dba
Eugene Markovskyi <eugene.markovskyi@duallab.com>
2012-04-06 11:28:45 +0000

    Improve cloneShallow logic. Some regression fixed. XMLWorkerHelper defaultCssFile should be used only for default.css. Cause incorrect results in unit tests. XMLWorker interface changed.

--- src/extras/itextsharp.xmlworker.testApps/html2pdf/Html2Pdf.cs
+++ src/extras/itextsharp.xmlworker.testApps/html2pdf/Html2Pdf.cs
@@ -6,6 +6,27 @@ using iTextSharp.text.pdf;
 using iTextSharp.tool.xml;
 
 namespace html2pdf {
+    class UnembedFontProvider : FontFactoryImp {
+        private String _baseFontEncoding = BaseFont.CP1252;
+
+        public UnembedFontProvider() : this(null, null) { }
+        public UnembedFontProvider(String fontsPath) : this(fontsPath, null) { }
+        public UnembedFontProvider(String fontsPath, String fontEncoding) {
+            if (fontEncoding != null) {
+                _baseFontEncoding = fontEncoding;
+            }
+            if (string.IsNullOrEmpty(fontsPath)) {
+                base.RegisterDirectories();
+            } else if (!fontsPath.Equals(XMLWorkerFontProvider.DONTLOOKFORFONTS)) {
+                RegisterDirectory(fontsPath, true);
+            }
+        }
+
+        public override Font GetFont(String fontname, String encoding, bool embedded, float size, int style, BaseColor color) {
+            return base.GetFont(fontname, _baseFontEncoding, false, size, style, color);
+        }
+    }
+
     class Html2Pdf {
         static void Main(string[] args) {
             if (args.Length < 2) {
@@ -16,11 +37,11 @@ namespace html2pdf {
 
             Document doc = new Document(PageSize.LETTER);
             String path = Path.GetDirectoryName(Path.GetFullPath(args[0])) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(args[0]) + ".pdf";
-            PdfWriter instance = PdfWriter.GetInstance(doc, new FileStream(path, FileMode.Create));
+            PdfWriter pdfWriter = PdfWriter.GetInstance(doc, new FileStream(path, FileMode.Create));
 
             doc.Open();
             XMLWorkerHelper.GetInstance()
-                .ParseXHtml(instance, doc, new FileStream(args[0], FileMode.Open), new FileStream(args[1], FileMode.Open), null);
+                .ParseXHtml(pdfWriter, doc, new FileStream(args[0], FileMode.Open), new FileStream(args[1], FileMode.Open), new UnembedFontProvider());
             doc.Close();
         }
     }
