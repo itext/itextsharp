cef36f00b5e94e92ebff106973effd9a5410b58f
Alexander Chingarev <alexander.chingarev@itextpdf.com>
2012-09-27 16:02:46 +0000

    Preparing for 5.3.3/1.2.0 release

--- src/core/AssemblyInfo.cs
+++ src/core/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.3.2")]
+[assembly: AssemblyVersion("5.3.3")]
