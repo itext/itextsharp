59e8d00e405dd2cb9c90f16af9e8e2d1a88c19d3
Pavel Alay <pavel.alay@duallab.com>
2013-04-08 14:47:35 +0000

    Add tabstop files

---
+++ src/core/iTextSharp/text/TabSplitCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iTextSharp.text.pdf;
+
+namespace iTextSharp.text
+{
+    public class TabSplitCharacter : ISplitCharacter
+    {
+        public static readonly ISplitCharacter TAB = new TabSplitCharacter();
+
+        public bool IsSplitCharacter(int start, int current, int end, char[] cc, PdfChunk[] ck)
+        {
+            return true;
+        }
+    }
+}
