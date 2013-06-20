7d183ba24e8d9924081d9796af860f37ea9012e2
Pavel Alay <pavel.alay@duallab.com>
2013-06-20 15:50:09 +0000

    Port MakeXmlSignature; Add XmlDSig tests;

--- src/core/iTextSharp/text/pdf/security/MakeXmlSignature.cs
+++ src/core/iTextSharp/text/pdf/security/MakeXmlSignature.cs
@@ -1,7 +1,14 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
-using Org.BouncyCastle.Crypto.Tls;
+using System.Xml;
+using System.Xml.XPath;
+using Org.BouncyCastle.X509;
+using iTextSharp.text.error_messages;
+using iTextSharp.text.exceptions;
+
 /*
  * $Id: MakeXmlSignature.java 5830 2013-05-31 09:29:15Z blowagie $
  *
@@ -52,6 +59,35 @@ namespace iTextSharp.text.pdf.security
      */
     public class MakeXmlSignature
     {
+        private const string XML_DSIG_URI = "http://www.w3.org/2000/09/xmldsig#";
+        private const string XML_DSIG_URI_C14N = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
+        private const string XML_DSIG_URI_RSA_SHA1 = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
+        private const string XML_DSIG_URI_DSA_SHA1 = "http://www.w3.org/2000/09/xmldsig#dsa-sha1";
+        private const string XML_DSIG_URI_ENVELOPED = "http://www.w3.org/2000/09/xmldsig#enveloped-signature";
+        private const string XML_DSIG_URI_XPATH_FILTER2 = "http://www.w3.org/2002/06/xmldsig-filter2";
+        private const string XML_DSIG_URI_SHA1 = "http://www.w3.org/2000/09/xmldsig#sha1";
+
+        private static void NormalizeNamespaces(XPathNavigator src, XPathNavigator dest)
+        {
+            IDictionary<string, string> dictLocal = src.GetNamespacesInScope(XmlNamespaceScope.Local);
+            IDictionary<string, string> dictExclude = src.GetNamespacesInScope(XmlNamespaceScope.ExcludeXml);
+
+            foreach (KeyValuePair<string, string> pair in dictExclude)
+                if (!dictLocal.ContainsKey(pair.Key))
+                    dest.CreateAttribute("xmlns", pair.Key, "http://www.w3.org/2000/xmlns/", pair.Value);
+        }
+
+        private static byte[] CalculateC14nByteRange(XmlDocument doc) {
+            XmlDsigC14NTransform c14nTransform = new XmlDsigC14NTransform();
+            c14nTransform.LoadInput(doc);
+            return ((MemoryStream)c14nTransform.GetOutput()).ToArray();
+        }
+
+
+        private static byte[] CalculateC14nDigest(XmlDocument doc, HashAlgorithm hash) {
+            return hash.ComputeHash(CalculateC14nByteRange(doc));
+        }
+
         /**
          * Signs the xml using the enveloped mode, with optional xpath transform (see XmlSignatureAppearance).
          * @param sap the XmlSignatureAppearance
@@ -61,12 +97,111 @@ namespace iTextSharp.text.pdf.security
          * @throws IOException
          * @throws DocumentException
          */
-        public static void signXmlDSig(XmlSignatureAppearance sap,
+        public static void SignXmlDSig(XmlSignatureAppearance sap,
             IExternalSignature externalSignature, KeyInfoClause keyInfo) {
-                throw new NotImplementedException("Xml signatures are not supported yet");
+            IXmlLocator xmlLocator = sap.GetXmlLocator();
+            if (xmlLocator == null)
+                throw new DocumentException(MessageLocalization.GetComposedMessage("xmllocator.cannot.be.null"));
+            if (!externalSignature.GetHashAlgorithm().Equals("SHA1"))
+                throw new UnsupportedPdfException(MessageLocalization.GetComposedMessage("support.only.sha1.hash.algorithm"));
+
+            XmlDocument originalDoc = xmlLocator.GetDocument();
+            byte[] md = CalculateC14nDigest(originalDoc, new SHA1Managed());
+            IXpathConstructor xpathConstructor = sap.GetXpathConstructor();
+
+
+            XmlElement signature = originalDoc.CreateElement("Signature", XML_DSIG_URI);
+            originalDoc.DocumentElement.AppendChild(signature);
+
+            XmlElement signedInfo = originalDoc.CreateElement("SignedInfo", XML_DSIG_URI);
+            signature.AppendChild(signedInfo);
+
+            XmlElement canonicalizationMethod = originalDoc.CreateElement("CanonicalizationMethod", XML_DSIG_URI);
+            canonicalizationMethod.SetAttribute("Algorithm", XML_DSIG_URI_C14N);
+            signedInfo.AppendChild(canonicalizationMethod);
+
+            XmlElement signatureMethod = originalDoc.CreateElement("SignatureMethod", XML_DSIG_URI);
+            if(externalSignature.GetEncryptionAlgorithm().Equals("RSA"))
+                signatureMethod.SetAttribute("Algorithm", XML_DSIG_URI_RSA_SHA1);
+            else if(externalSignature.GetEncryptionAlgorithm().Equals("DSA"))
+                signatureMethod.SetAttribute("Algorithm", XML_DSIG_URI_DSA_SHA1);
+            else
+                throw new UnsupportedPdfException(MessageLocalization.GetComposedMessage("support.only.rsa.and.dsa.algorithms"));
+            signedInfo.AppendChild(signatureMethod);
+
+            XmlElement reference = originalDoc.CreateElement("Reference", XML_DSIG_URI);
+            reference.SetAttribute("URI", "");
+
+            XmlElement transforms = originalDoc.CreateElement("Transforms", XML_DSIG_URI);
+
+            XmlElement transform = originalDoc.CreateElement("Transform", XML_DSIG_URI);
+            transform.SetAttribute("Algorithm", XML_DSIG_URI_ENVELOPED);
+            transforms.AppendChild(transform);
+
+            if (xpathConstructor != null && xpathConstructor.GetXpathExpression().Length > 0) {
+                XmlNodeList nodelist = originalDoc.SelectNodes(xpathConstructor.GetXpathExpression(), xpathConstructor.GetNamespaceManager());
+                if (nodelist.Count == 1 && nodelist[0].NodeType == XmlNodeType.Element) {
+                    XmlElement xpathSelect = (XmlElement) nodelist[0].CloneNode(true);
+                    NormalizeNamespaces(nodelist[0].CreateNavigator(), xpathSelect.CreateNavigator());
+
+                    XmlDocument digestDoc = new XmlDocument(originalDoc.NameTable);
+                    digestDoc.LoadXml(xpathSelect.OuterXml);
+
+                    md = CalculateC14nDigest(digestDoc, new SHA1Managed());
+
+                    transform = originalDoc.CreateElement("Transform", XML_DSIG_URI);
+                    transform.SetAttribute("Algorithm", XML_DSIG_URI_XPATH_FILTER2);
+
+                    XmlElement xpath = originalDoc.CreateElement("XPath", XML_DSIG_URI);
+                    xpath.SetAttribute("xmlns", XML_DSIG_URI_XPATH_FILTER2);
+                    xpath.SetAttribute("Filter", "intersect");
+
+                    XmlNode xpathNode = originalDoc.CreateTextNode(xpathConstructor.GetXpathExpression());
+                    
+                    xpath.AppendChild(xpathNode);
+                    transform.AppendChild(xpath);
+                    transforms.AppendChild(transform);
+                }
+            }
+            
+            reference.AppendChild(transforms);
+            
+            XmlElement digestMethod = originalDoc.CreateElement("DigestMethod", XML_DSIG_URI);
+            digestMethod.SetAttribute("Algorithm", XML_DSIG_URI_SHA1);
+            reference.AppendChild(digestMethod);
+
+            XmlElement digestValue = originalDoc.CreateElement("DigestValue", XML_DSIG_URI);
+            
+            digestValue.AppendChild(originalDoc.CreateTextNode(Convert.ToBase64String(md)));
+            
+            reference.AppendChild(digestValue);
+            signedInfo.AppendChild(reference);
+
+            XmlElement signedInfoDigest = (XmlElement)signedInfo.CloneNode(true);
+            NormalizeNamespaces(signedInfo.CreateNavigator(), signedInfoDigest.CreateNavigator());
+            XmlDocument signedInfoDoc = new XmlDocument(originalDoc.NameTable);
+            signedInfoDoc.LoadXml(signedInfoDigest.OuterXml);
+            byte[] byteRange = CalculateC14nByteRange(signedInfoDoc);
+
+            //Sign with ExternalSignature
+            String valueBase64 = Convert.ToBase64String(externalSignature.Sign(byteRange));
+
+            XmlElement signatureValue = originalDoc.CreateElement("SignatureValue", XML_DSIG_URI);
+            signatureValue.AppendChild(originalDoc.CreateTextNode(valueBase64));
+
+            signature.AppendChild(signatureValue);
+
+            if (keyInfo != null) {
+                XmlElement keyInfoElement = originalDoc.CreateElement("KeyInfo", XML_DSIG_URI);
+                keyInfoElement.AppendChild(originalDoc.ImportNode(keyInfo.GetXml(), true));
+                signature.AppendChild(keyInfoElement);
+            }
+                
+            xmlLocator.SetDocument(originalDoc);
+            sap.Close();    
         }
 
-        /**
+         /**
          * Signs the xml using the enveloped mode, with optional xpath transform (see XmlSignatureAppearance).
          * @param sap the XmlSignatureAppearance
          * @param externalSignature  the interface providing the actual signing
@@ -75,9 +210,9 @@ namespace iTextSharp.text.pdf.security
          * @throws IOException
          * @throws DocumentException
          */
-        public static void signXmlDSig(XmlSignatureAppearance sap,
-            IExternalSignature externalSignature, Certificate[] chain) {
-                throw new NotImplementedException("Xml signatures are not supported yet");
+        public static void SignXmlDSig(XmlSignatureAppearance sap,
+            IExternalSignature externalSignature, X509Certificate[] chain) {
+            SignXmlDSig(sap, externalSignature, new KeyInfoX509Data(chain[0].GetEncoded()));
         }
 
         /**
@@ -89,9 +224,14 @@ namespace iTextSharp.text.pdf.security
          * @throws IOException
          * @throws DocumentException
          */
-        public static void signXmlDSig(XmlSignatureAppearance sap,
-            IExternalSignature externalSignature, PublicKey publicKey) {
-                throw new NotImplementedException("Xml signatures are not supported yet");
-        } 
+        public static void SignXmlDSig(XmlSignatureAppearance sap,
+            IExternalSignature externalSignature, AsymmetricAlgorithm publicKey) {
+            if (publicKey is DSA)
+                SignXmlDSig(sap, externalSignature, new DSAKeyValue((DSA)publicKey));
+            else if(publicKey is RSA)
+                SignXmlDSig(sap, externalSignature, new RSAKeyValue((RSA)publicKey));
+            else
+                throw new UnsupportedPdfException(MessageLocalization.GetComposedMessage("support.only.rsa.and.dsa.algorithms"));
+        }
     }
 }
