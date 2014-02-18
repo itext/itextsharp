216602cf1c3d802eb41b575c6008d525e7fc2cd6
Alexey Subach <alexey.subach@itextpdf.com>
2014-02-18 20:10:45 +0000

    DEV-853 Script for automatic assembling release of iTextSharp zip package.

---
+++ src/BuildAll.bat
@@ -0,0 +1,150 @@
+@echo off
+
+if not defined msbuildExec (
+	set msbuildExec=msbuild
+)
+
+if not defined tortoiseProcExec (
+	set tortoiseProcExec=tortoiseProc
+)
+
+if not defined sevenZipExec (
+	set sevenZipExec=7z
+)
+
+
+echo Cleaning up...
+
+rem update
+rem call TortoiseProc.exe /command:update /path:%~dp0 /closeonend:0
+
+rem cleanup   /revert  /refreshshell
+call "%tortoiseProcExec%" /command:cleanup /nodlg /noui /noprogressui /path:%~dp0 /cleanup /delunversioned /delignored /externals
+if errorlevel 1 goto cleanUpFailed
+
+
+
+echo Creating archives with sources...
+
+rem zip itextsharp-src-core.zip
+call "%sevenZipExec%" a -tzip itextsharp-src-core.zip -r .\core\* > nul
+if errorlevel 1 goto archiveFailed
+
+rem zip itextsharp-src-pdfa.zip
+call "%sevenZipExec%" a -tzip itextsharp-src-pdfa.zip -r .\extras\itextsharp.pdfa\* > nul
+if errorlevel 1 goto archiveFailed
+
+rem zip itextsharp-src-xtra.zip
+call "%sevenZipExec%" a -tzip itextsharp-src-xtra.zip -r .\extras\iTextSharp.xtra\* > nul
+if errorlevel 1 goto archiveFailed
+
+
+
+echo Building binaries...
+
+rem rebuild core Release
+call "%msbuildExec%" core\itextsharp.csproj /t:Rebuild /p:Configuration=Release > nul
+if errorlevel 1 goto buildFailed
+
+rem rebuild core Release_woDrawing
+call "%msbuildExec%" core\itextsharp.csproj /t:Rebuild /p:Configuration=Release_woDrawing > nul
+if errorlevel 1 goto buildFailed
+
+rem rebuild pdfa Release
+call "%msbuildExec%" extras\itextsharp.pdfa\itextsharp.pdfa.csproj /t:Rebuild /p:Configuration=Release > nul
+if errorlevel 1 goto buildFailed
+
+rem rebuild xtra Release
+call "%msbuildExec%" extras\iTextSharp.xtra\iTextSharp.xtra.csproj /t:Rebuild /p:Configuration=Release > nul
+if errorlevel 1 goto buildFailed
+
+
+
+echo Creating archives with binaries...
+
+rem zip core dll Release itextsharp-dll-core.zip
+call "%sevenZipExec%" a -tzip itextsharp-dll-core.zip -r .\core\bin\Release\* > nul
+if errorlevel 1 goto archiveFailed
+
+rem zip core dll Release_woDrawing itextsharp-dll-core-wo_Drawing.zip
+call "%sevenZipExec%" a -tzip itextsharp-dll-core-wo_Drawing.zip -r .\core\bin\Release_woDrawing\* > nul
+if errorlevel 1 goto archiveFailed
+
+rem zip pdfa dll Release itextsharp-dll-pdfa.zip
+call "%sevenZipExec%" a -tzip itextsharp-dll-pdfa.zip .\extras\itextsharp.pdfa\bin\Release\itextsharp.pdfa.dll > nul
+if errorlevel 1 goto archiveFailed
+
+rem zip xtra dll Release itextsharp-dll-xtra.zip
+call "%sevenZipExec%" a -tzip itextsharp-dll-xtra.zip .\extras\iTextSharp.xtra\bin\Release\itextsharp.xtra.dll > nul
+if errorlevel 1 goto archiveFailed
+
+
+
+echo Creating resulting archive...
+
+rem add to result  \itext\trunk\src\core\iTextSharp\text\NOTICE.txt 
+call "%sevenZipExec%" a -tzip itextsharp-all.zip .\core\iTextSharp\text\NOTICE.txt > nul
+if errorlevel 1 goto archiveFailed
+
+rem add to result archives
+call "%sevenZipExec%" a -tzip itextsharp-all.zip itextsharp-dll-core.zip itextsharp-dll-core-wo_Drawing.zip itextsharp-dll-pdfa.zip itextsharp-dll-xtra.zip itextsharp-src-core.zip itextsharp-src-pdfa.zip itextsharp-src-xtra.zip > nul
+if errorlevel 1 goto archiveFailed
+
+rem delete temp archives
+call :deleteTempFiles
+
+
+
+echo Success!
+pause
+exit
+
+
+
+:buildFailed
+echo:
+echo Binaries build failed. Please, make sure that either msbuildExec path variable is defined or msbuild.exe is available to call from command line. The process will be aborted.
+echo:
+call :deleteTempFiles
+pause
+exit
+
+:cleanUpFailed
+echo:
+echo Cleaning up failed. Please, make sure that either tortoiseProcExec path variable is defined or tortoiseProc.exe is available to call from command line. The process will be aborted.
+echo:
+pause
+exit
+
+:archiveFailed
+echo:
+echo Archive creating failed. Please, make sure that either sevenZipExec path variable is defined or 7z.exe is available to call from command line. The process will be aborted.
+echo:
+call :deleteTempFiles
+pause
+exit
+
+:deleteTempFiles
+echo Deleting temporarily created zip files...
+if exist itextsharp-dll-core.zip (
+	del itextsharp-dll-core.zip
+)
+if exist itextsharp-dll-core-wo_Drawing.zip (
+	del itextsharp-dll-core-wo_Drawing.zip
+)
+if exist itextsharp-dll-pdfa.zip (
+	del itextsharp-dll-pdfa.zip
+)
+if exist itextsharp-dll-xtra.zip (
+	del itextsharp-dll-xtra.zip
+)
+if exist itextsharp-src-core.zip (
+	del itextsharp-src-core.zip
+)
+if exist itextsharp-src-pdfa.zip (
+	del itextsharp-src-pdfa.zip
+)
+if exist itextsharp-src-xtra.zip (
+	del itextsharp-src-xtra.zip
+)
+exit /b
