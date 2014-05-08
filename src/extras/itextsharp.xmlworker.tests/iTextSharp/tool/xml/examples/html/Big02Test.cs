3dac2493d918396b86430feef3570d3fb155f62a
Alexey Subach <alexey.subach@itextpdf.com>
2014-05-08 13:14:12 +0000

    Port XmlWorker revision 455 (forgot the test classes for the previous commit)

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/html/Big02Test.cs
@@ -0,0 +1,7 @@
+﻿namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.html {
+    public class Big02Test : SampleTest {
+        protected override string GetTestName() {
+            return "big02";
+        }
+    }
+}
