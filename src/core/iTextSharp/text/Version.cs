59ae05c2a7d16057227aa1fd3a2dc69a2258566c
Raf Hens <raf.hens@itextpdf.com>
2014-05-15 14:26:04 +0000

    Producer line: 2013 -> 2014

--- src/core/iTextSharp/text/Version.cs
+++ src/core/iTextSharp/text/Version.cs
@@ -81,7 +81,7 @@ namespace iTextSharp.text {
 	     * iText Group requests that you retain the iText producer line
 	     * in every PDF that is created or manipulated using iText.
 	     */
-	    private String iTextVersion = iText + " " + release + " \u00a92000-2013 iText Group NV";
+	    private String iTextVersion = iText + " " + release + " \u00a92000-2014 iText Group NV";
 
 	    /** The iText version instance. */
 	    private static Version version = null;
