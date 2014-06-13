059d6b6a2488ae88a4288baa04390d45443bd1b2
Alexey Subach <alexey.subach@itextpdf.com>
2014-06-13 11:36:45 +0000

    Port XmlWorker revision 465 (XmlWorker: add support of attribute css selector.).

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/css/selector/Css3Modsel_184cTest.cs
@@ -0,0 +1,7 @@
+namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.css.selector {
+    public class Css3Modsel_184cTest : SampleTest {
+        protected override string GetTestName() {
+            return "css3-modsel-184c";
+        }
+    }
+}
