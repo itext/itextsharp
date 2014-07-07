32ee78ab297fcaace46229c8d9f59722d58763af
Alexey Subach <alexey.subach@itextpdf.com>
2014-07-07 13:52:46 +0000

    Port XmlWorker revisions 467 (SUP-538 Underlined and striked through text.), 468 (Fix test fail: ignore a css selector test.).

--- src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/css/selector/Css3Modsel_170bTest.cs
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/css/selector/Css3Modsel_170bTest.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
+
 namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.css.selector {
+    [Ignore("http://ci.itextsupport.com/job/xmlworker-trunk/com.itextpdf.tool$xmlworker/993/testReport/com.itextpdf.tool.xml.examples.css.selector/Css3Modsel_170bTest/test/")]
     public class Css3Modsel_170bTest : SampleTest {
         protected override string GetTestName() {
             return "css3-modsel-170b";
