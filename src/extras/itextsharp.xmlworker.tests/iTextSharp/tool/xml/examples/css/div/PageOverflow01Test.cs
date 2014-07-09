ef258212b1b34db86a7dd83b2d9c4487cf89ff9b
Alexey Subach <alexey.subach@itextpdf.com>
2014-07-09 15:12:59 +0000

    Port XmlWorker revisions 469 (DEV-980 Div width is not limitted by page width from HtmlPipelineContext), 470 (DEV-980 Div width is not limitted by page width from HtmlPipelineContext. Incorrect change reverted.), 471 (SUP-539 Text quotation (blockquote tag).), 472 (Revert changes from revisions 469-470, ignore PageOverflow01Test test which was added in those revisions. Unignore div tests from maven configuration.).

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/css/div/PageOverflow01Test.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.tool.xml.html;
+using iTextSharp.tool.xml.parser;
+using iTextSharp.tool.xml.pipeline.css;
+using iTextSharp.tool.xml.pipeline.end;
+using iTextSharp.tool.xml.pipeline.html;
+using NUnit.Framework;
+
+namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.css.div {
+    [Ignore]
+    public class PageOverflow01Test : SampleTest {
+        protected override string GetTestName() {
+            return "pageOverflow01";
+        }
+
+        protected override void MakePdf(string outPdf) {
+            Document doc = new Document(PageSize.A4.Rotate());
+            PdfWriter pdfWriter = PdfWriter.GetInstance(doc, new FileStream(outPdf, FileMode.Create));
+            doc.Open();
+            TransformHtml2Pdf(doc, pdfWriter, new SampleTestImageProvider(), new XMLWorkerFontProvider(RESOURCES + @"tool\xml\examples\fonts"),
+                new FileStream(RESOURCES + @"tool\xml\examples\sampleTest.css", FileMode.Open));
+            doc.Close();
+        }
+
+        protected override void TransformHtml2Pdf(Document doc, PdfWriter pdfWriter, IImageProvider imageProvider,
+            IFontProvider fontProvider, Stream cssFile) {
+            ICSSResolver cssResolver = XMLWorkerHelper.GetInstance().GetDefaultCssResolver(true);
+
+            HtmlPipelineContext hpc;
+
+            if (fontProvider != null)
+                hpc = new HtmlPipelineContext(new CssAppliersImpl(fontProvider));
+            else
+                hpc = new HtmlPipelineContext(null);
+
+            hpc.SetImageProvider(imageProvider);
+            hpc.SetAcceptUnknown(true).AutoBookmark(true).SetTagFactory(Tags.GetHtmlTagProcessorFactory());
+            HtmlPipeline htmlPipeline = new HtmlPipeline(hpc, new PdfWriterPipeline(doc, pdfWriter));
+            IPipeline pipeline = new CssResolverPipeline(cssResolver, htmlPipeline);
+            XMLWorker worker = new XMLWorker(pipeline, true);
+            XMLParser xmlParse = new XMLParser(true, worker, Encoding.UTF8);
+            xmlParse.Parse(new FileStream(inputHtml, FileMode.Open), Encoding.UTF8);
+        }
+    }
+}
---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/html/Blockquote01Test.cs
@@ -0,0 +1,7 @@
+﻿namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.html {
+    public class Blockquote01Test : SampleTest {
+        protected override string GetTestName() {
+            return "blockquote01";
+        }
+    }
+}
