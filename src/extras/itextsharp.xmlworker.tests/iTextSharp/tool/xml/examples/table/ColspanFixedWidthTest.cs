66ddc51c3e5e4c094c93061a0af18122e9ba22d9
Alexey Subach <alexey.subach@itextpdf.com>
2015-01-08 07:51:29 +0000

    Port XmlWorker revision 517 (DEV-1043 Table colspan + fixed widths error.).

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/table/ColspanFixedWidthTest.cs
@@ -0,0 +1,7 @@
+﻿namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.table {
+    public class ColspanFixedWidthTest : SampleTest {
+        protected override string GetTestName() {
+            return "colspan_fixed_width";
+        }
+    }
+}
