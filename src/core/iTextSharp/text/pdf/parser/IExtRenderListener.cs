dda5c39680143f9459cf04297ecb38c1cdd19e76
Pavel Mitrofanov <pavel.mitrofanov@duallab.com>
2015-04-01 13:26:06 +0300

    Ported Rev. 674f271, Rev. 7e57e31, Rev. a9b659d, Rev. caea180 (Added classes for path abstraction; Modified PdfContentStreamProcessor to be able to process path construction and painting, extended existing render listener)

---
+++ src/core/iTextSharp/text/pdf/parser/IExtRenderListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTextSharp.text.pdf.parser {
+
+    /**
+     * Simply extends the {@link com.itextpdf.text.pdf.parser.RenderListener} interface to provide
+     * additional methods.
+     *
+     * {@inheritDoc}
+     *
+     * @since 5.5.6
+     */
+    public interface IExtRenderListener : IRenderListener {
+
+        /**
+         * Called when the current path is being modifying. E.g. new segment is being added,
+         * new subpath is being started etc.
+         *
+         * @param renderInfo Contains information about the path segment being added to the current path.
+         */
+        void ModifyPath(PathConstructionRenderInfo renderInfo);
+
+        /**
+         * Called when the current path should be rendered.
+         *
+         * @param renderInfo Contains information about the current path which should be rendered.
+         * @return The path which can be used as a new clipping path.
+         */
+        Path RenderPath(PathPaintingRenderInfo renderInfo);
+
+        /**
+         * Called when the current path should be set as a new clipping path.
+         *
+         * @param rule Either {@link PathPaintingRenderInfo#EVEN_ODD_RULE} or {@link PathPaintingRenderInfo#NONZERO_WINDING_RULE}
+         */
+        void ClipPath(int rule);
+    }
+}
