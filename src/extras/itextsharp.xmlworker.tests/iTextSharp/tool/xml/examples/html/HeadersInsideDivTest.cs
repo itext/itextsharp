5ec274eb7ad63a6a461032b6f1930c8839896709
Ryan Hamer <ryan.hamer@itextpdf.com>
2015-05-21 09:25:23 -0400

    DEV-1412 - Port of DEV-1402.  Heading tags inside of div tag now correctly create bookmarks automatically.

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/html/HeadersInsideDivTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.html
+{
+    internal class HeadersInsideDivTest : SampleTest
+    {
+        protected override string GetTestName()
+        {
+            return "headersInDiv";
+        }
+    }
+}
