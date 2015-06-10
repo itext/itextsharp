7bc0183f5a17376eb792f4759785c18733007555
Trusevich <dmitry.trusevich@duallab>
2015-06-10 17:21:41 +0300

    Port: When null is read as a PdfLiteral, it isn't recognized as a PdfNull object
    (DEV-1433)

--- src/core/iTextSharp/text/pdf/PdfReader.cs
+++ src/core/iTextSharp/text/pdf/PdfReader.cs
@@ -2355,7 +2355,7 @@ namespace iTextSharp.text.pdf {
                     PdfObject dpEntry = GetPdfObject(dp[j]);
                     if (dpEntry is PdfDictionary){
                         decodeParams = (PdfDictionary)dpEntry;
-                    } else if (dpEntry == null || dpEntry is PdfNull) {
+                    } else if (dpEntry == null || dpEntry is PdfNull || (dpEntry is PdfLiteral && EqualBytes(Encoding.UTF8.GetBytes("null"), ((PdfLiteral)dpEntry).GetBytes()))) {
                         decodeParams = null;
                     } else {
                         throw new UnsupportedPdfException(MessageLocalization.GetComposedMessage("the.decode.parameter.type.1.is.not.supported", dpEntry.GetType().FullName));
@@ -2627,7 +2627,20 @@ namespace iTextSharp.text.pdf {
                 return decrypt;
             }
         }
-        
+
+        internal static bool EqualBytes(byte[] a1, byte[] b1) {
+            if (a1.Length != b1.Length) {
+                return false;
+            }
+            for (int i = 0; i < a1.Length; i++) {
+                if (a1[i] != b1[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //possible IndexOutException 
         internal static bool Equalsn(byte[] a1, byte[] a2) {
             int length = a2.Length;
             for (int k = 0; k < length; ++k) {
