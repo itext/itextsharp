f1ff1bca7efe22c3e1748779edc378d724db3a73
Dmitry Trusevich <dmitry.trusevich@duallab.com>
2015-11-26 13:32:39 +0300

    port: XmlWorker: Ordered List Alphabetical symbol
    Commits: itext/xmlworker [1aaff941]
    
    Resolved: DEV-1618
    Ported: DEV-1620

---
+++ src/extras/itextsharp.xmlworker.tests/iTextSharp/tool/xml/examples/html/LetteredListTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+
+namespace itextsharp.xmlworker.tests.iTextSharp.tool.xml.examples.html
+{
+    class LetteredListTest : SampleTest {
+
+        protected override string GetTestName() {
+            return "letteredList";
+        }
+
+        protected override void MakePdf(string outPdf) {
+
+            Document doc = new Document(PageSize.A4);
+            PdfWriter pdfWriter = PdfWriter.GetInstance(doc, new FileStream(outPdf, FileMode.Create));
+            doc.Open();
+            FileStream cssFileStream = new FileStream(RESOURCES + @"\tool\xml\examples\html\letteredList\" + "sampleTest.css", FileMode.Open, FileAccess.Read, FileShare.Read);
+            TransformHtml2Pdf(doc, pdfWriter, new SampleTestImageProvider(), new XMLWorkerFontProvider(RESOURCES + @"\tool\xml\examples\fonts"), cssFileStream);
+            doc.Close();
+
+        }
+    }
+}
