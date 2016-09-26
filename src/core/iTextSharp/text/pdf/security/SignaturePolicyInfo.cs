5c28700877c5b1d20263fe371c3ef58c86d1ca1f
Alexey Subach <alexey.subach@duallab.com>
2016-09-26 13:04:52 +0300

    Fix porting for lower C# versions

--- src/core/iTextSharp/text/pdf/security/SignaturePolicyInfo.cs
+++ src/core/iTextSharp/text/pdf/security/SignaturePolicyInfo.cs
@@ -14,7 +14,13 @@ namespace iTextSharp.text.pdf.security {
      *      SignaturePolicyInfo spi = new SignaturePolicyInfo("2.16.724.1.3.1.1.2.1.9", 
      *      "G7roucf600+f03r/o0bAOQ6WAs0=", "SHA-1", "https://sede.060.gob.es/politica_de_firma_anexo_1.pdf");
      */
+
     public class SignaturePolicyInfo {
+        private string policyIdentifier;
+        private byte[] policyHash;
+        private string policyDigestAlgorithm;
+        private string policyUri;
+
         public SignaturePolicyInfo(string policyIdentifier, byte[] policyHash, string policyDigestAlgorithm, string policyUri) {
             if (string.IsNullOrEmpty(policyIdentifier)) {
                 throw new ArgumentException("Policy identifier cannot be null");
@@ -26,23 +32,33 @@ namespace iTextSharp.text.pdf.security {
                 throw new ArgumentException("Policy digest algorithm cannot be null");
             }
 
-            PolicyIdentifier = policyIdentifier;
-            PolicyHash = policyHash;
-            PolicyDigestAlgorithm = policyDigestAlgorithm;
-            PolicyUri = policyUri;
+            this.policyIdentifier = policyIdentifier;
+            this.policyHash = policyHash;
+            this.policyDigestAlgorithm = policyDigestAlgorithm;
+            this.policyUri = policyUri;
         }
 
-        public SignaturePolicyInfo(string policyIdentifier, string policyHashBase64, string policyDigestAlgorithm, string policyUri) :
-            this(policyIdentifier, policyHashBase64 != null ? Convert.FromBase64String(policyHashBase64) : null, policyDigestAlgorithm, policyUri) {
+        public SignaturePolicyInfo(string policyIdentifier, string policyHashBase64, string policyDigestAlgorithm,
+            string policyUri) : this(
+                policyIdentifier, policyHashBase64 != null ? Convert.FromBase64String(policyHashBase64) : null,
+                policyDigestAlgorithm, policyUri) {
         }
 
-        public string PolicyIdentifier { get; private set; }
+        public string PolicyIdentifier {
+            get { return policyIdentifier; }
+        }
 
-        public byte[] PolicyHash { get; private set; }
+        public byte[] PolicyHash {
+            get { return policyHash; }
+        }
 
-        public string PolicyDigestAlgorithm { get; private set; } 
+        public string PolicyDigestAlgorithm {
+            get { return policyDigestAlgorithm; }
+        }
 
-        public string PolicyUri { get; private set; }
+        public string PolicyUri {
+            get { return policyUri; }
+        }
 
         protected internal SignaturePolicyIdentifier ToSignaturePolicyIdentifier() {
             string algId = DigestAlgorithms.GetAllowedDigests(this.PolicyDigestAlgorithm);
@@ -58,8 +74,9 @@ namespace iTextSharp.text.pdf.security {
                 spqi = new SigPolicyQualifierInfo(PkcsObjectIdentifiers.IdSpqEtsUri, new DerIA5String(this.PolicyUri));
             }
 
-            signaturePolicyIdentifier = new SignaturePolicyIdentifier(new SignaturePolicyId(DerObjectIdentifier.GetInstance(new DerObjectIdentifier(this.PolicyIdentifier.Replace("urn:oid:", ""))),
-                new OtherHashAlgAndValue(new AlgorithmIdentifier(algId), new DerOctetString(this.PolicyHash)), spqi));
+            signaturePolicyIdentifier = new SignaturePolicyIdentifier(new SignaturePolicyId(
+                        DerObjectIdentifier.GetInstance(new DerObjectIdentifier(this.PolicyIdentifier.Replace("urn:oid:", ""))),
+                        new OtherHashAlgAndValue(new AlgorithmIdentifier(algId), new DerOctetString(this.PolicyHash)), spqi));
 
             return signaturePolicyIdentifier;
         }
