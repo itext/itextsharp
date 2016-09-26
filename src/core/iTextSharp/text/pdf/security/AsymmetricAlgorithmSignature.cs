bf89703327c16c1df01c0d1ff905ce6c4a4a9fa8
Alexey Subach <alexey.subach@duallab.com>
2016-09-26 15:01:34 +0300

    Add possibility to use workaround for SHA-256 and SHA-512 for RSACryptoServiceProvider
    Workaround should be done at the user side, and then RSACryptoServiceProvider can be passed to AsymmetricAlgorithmSignature
    
    Original suggestion by J. Arturo
    
    DEV-1806

---
+++ src/core/iTextSharp/text/pdf/security/AsymmetricAlgorithmSignature.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace iTextSharp.text.pdf.security {
+    public class AsymmetricAlgorithmSignature : IExternalSignature {
+        private AsymmetricAlgorithm algorithm;
+        /** The hash algorithm. */
+        private String hashAlgorithm;
+        /** The encryption algorithm (obtained from the private key) */
+        private String encryptionAlgorithm;
+
+        private AsymmetricAlgorithmSignature(AsymmetricAlgorithm algorithm, String hashAlgorithm) {
+            this.algorithm = algorithm;
+            this.hashAlgorithm = DigestAlgorithms.GetDigest(DigestAlgorithms.GetAllowedDigests(hashAlgorithm));
+
+            if (algorithm is RSACryptoServiceProvider)
+                encryptionAlgorithm = "RSA";
+            else if (algorithm is DSACryptoServiceProvider)
+                encryptionAlgorithm = "DSA";
+            else
+                throw new ArgumentException("Not supported encryption algorithm " + algorithm);
+        }
+
+        public AsymmetricAlgorithmSignature(RSACryptoServiceProvider algorithm, String hashAlgorithm)
+            : this((AsymmetricAlgorithm) algorithm, hashAlgorithm) {
+        }
+
+        public AsymmetricAlgorithmSignature(DSACryptoServiceProvider algorithm)
+            : this((AsymmetricAlgorithm) algorithm, null) {
+        }
+
+        public byte[] Sign(byte[] message) {
+            if (algorithm is RSACryptoServiceProvider) {
+                RSACryptoServiceProvider rsa = (RSACryptoServiceProvider) algorithm;
+                return rsa.SignData(message, hashAlgorithm);
+            } else {
+                DSACryptoServiceProvider dsa = (DSACryptoServiceProvider) algorithm;
+                return dsa.SignData(message);
+            }
+        }
+
+        public string GetHashAlgorithm() {
+            return hashAlgorithm;
+        }
+
+        /**
+         * Returns the encryption algorithm used for signing.
+         * @return the encryption algorithm ("RSA" or "DSA")
+         * @see com.itextpdf.text.pdf.security.ExternalSignature#getEncryptionAlgorithm()
+         */
+
+        public string GetEncryptionAlgorithm() {
+            return encryptionAlgorithm;
+        }
+    }
+}
