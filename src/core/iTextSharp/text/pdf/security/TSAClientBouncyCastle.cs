c5f7679f79203b71fdde70f9306c0d921566c0e1
Filipe Gomes <filipemagomes@gmail.com>
2016-10-28 16:35:28 +0100

    The TSA server require a OID policy.

--- src/core/iTextSharp/text/pdf/security/TSAClientBouncyCastle.cs
+++ src/core/iTextSharp/text/pdf/security/TSAClientBouncyCastle.cs
@@ -59,7 +59,8 @@ using iTextSharp.text.error_messages;
  * address: sales@itextpdf.com
  */
 
-namespace iTextSharp.text.pdf.security {
+namespace iTextSharp.text.pdf.security
+{
 
     /**
     * Time Stamp Authority Client interface implementation using Bouncy Castle
@@ -70,14 +71,15 @@ namespace iTextSharp.text.pdf.security {
     * </p>
     * @since	2.1.6
     */
-    public class TSAClientBouncyCastle : ITSAClient {
+    public class TSAClientBouncyCastle : ITSAClient
+    {
 
         /** The Logger instance. */
         private static readonly ILogger LOGGER = LoggerFactory.GetLogger(typeof(TSAClientBouncyCastle));
 
         /** URL of the Time Stamp Authority */
-	    protected internal String tsaURL;
-	    /** TSA Username */
+        protected internal String tsaURL;
+        /** TSA Username */
         protected internal String tsaUsername;
         /** TSA password */
         protected internal String tsaPassword;
@@ -85,33 +87,52 @@ namespace iTextSharp.text.pdf.security {
         protected ITSAInfoBouncyCastle tsaInfo;
         /** The default value for the hash algorithm */
         public const int DEFAULTTOKENSIZE = 4096;
-        
+
         /** Estimate of the received time stamp token */
         protected internal int tokenSizeEstimate;
-        
+
         /** The default value for the hash algorithm */
         public const String DEFAULTHASHALGORITHM = "SHA-256";
-        
+
         /** Hash algorithm */
         protected internal String digestAlgorithm;
+
+        /** The default value for the tsa request policy */
+        private string tsaReqPolicy = null;
+
+        /** The tsa request policy */
+        public string TSAReqPolicy
+        {
+            private get
+            {
+                return tsaReqPolicy;
+            }
+            set
+            {
+                tsaReqPolicy = value;
+            }
+        }
+
         /**
         * Creates an instance of a TSAClient that will use BouncyCastle.
         * @param url String - Time Stamp Authority URL (i.e. "http://tsatest1.digistamp.com/TSA")
         */
-        public TSAClientBouncyCastle(String url) 
-            : this(url, null, null, DEFAULTTOKENSIZE, DEFAULTHASHALGORITHM) {
+        public TSAClientBouncyCastle(String url)
+            : this(url, null, null, DEFAULTTOKENSIZE, DEFAULTHASHALGORITHM)
+        {
         }
-        
+
         /**
         * Creates an instance of a TSAClient that will use BouncyCastle.
         * @param url String - Time Stamp Authority URL (i.e. "http://tsatest1.digistamp.com/TSA")
         * @param username String - user(account) name
         * @param password String - password
         */
-        public TSAClientBouncyCastle(String url, String username, String password) 
-            : this(url, username, password, DEFAULTTOKENSIZE, DEFAULTHASHALGORITHM) {
+        public TSAClientBouncyCastle(String url, String username, String password)
+            : this(url, username, password, DEFAULTTOKENSIZE, DEFAULTHASHALGORITHM)
+        {
         }
-        
+
         /**
         * Constructor.
         * Note the token size estimate is updated by each call, as the token
@@ -122,18 +143,20 @@ namespace iTextSharp.text.pdf.security {
         * @param password String - password
         * @param tokSzEstimate int - estimated size of received time stamp token (DER encoded)
         */
-        public TSAClientBouncyCastle(String url, String username, String password, int tokSzEstimate, String digestAlgorithm) {
-            this.tsaURL       = url;
-            this.tsaUsername  = username;
-            this.tsaPassword  = password;
+        public TSAClientBouncyCastle(String url, String username, String password, int tokSzEstimate, String digestAlgorithm)
+        {
+            this.tsaURL = url;
+            this.tsaUsername = username;
+            this.tsaPassword = password;
             this.tokenSizeEstimate = tokSzEstimate;
             this.digestAlgorithm = digestAlgorithm;
         }
-        
+
         /**
          * @param tsaInfo the tsaInfo to set
          */
-        virtual public void SetTSAInfo(ITSAInfoBouncyCastle tsaInfo) {
+        virtual public void SetTSAInfo(ITSAInfoBouncyCastle tsaInfo)
+        {
             this.tsaInfo = tsaInfo;
         }
 
@@ -142,15 +165,17 @@ namespace iTextSharp.text.pdf.security {
         * Returned value reflects the result of the last succesfull call, padded
         * @return an estimate of the token size
         */
-        public virtual int GetTokenSizeEstimate() {
+        public virtual int GetTokenSizeEstimate()
+        {
             return tokenSizeEstimate;
         }
-        
+
         /**
          * Gets the MessageDigest to digest the data imprint
          * @return the digest algorithm name
          */
-        virtual public IDigest GetMessageDigest() {
+        virtual public IDigest GetMessageDigest()
+        {
             return DigestAlgorithms.GetMessageDigest(digestAlgorithm);
         }
 
@@ -160,60 +185,72 @@ namespace iTextSharp.text.pdf.security {
          * @param imprint data imprint to be time-stamped
          * @return encoded, TSA signed data of the timeStampToken
          */
-        public virtual byte[] GetTimeStampToken(byte[] imprint) {
+        public virtual byte[] GetTimeStampToken(byte[] imprint)
+        {
             byte[] respBytes = null;
             // Setup the time stamp request
             TimeStampRequestGenerator tsqGenerator = new TimeStampRequestGenerator();
             tsqGenerator.SetCertReq(true);
+
+            if (!string.IsNullOrEmpty(this.TSAReqPolicy))
+            {
+                tsqGenerator.SetReqPolicy(TSAReqPolicy);
+            }
+
             // tsqGenerator.setReqPolicy("1.3.6.1.4.1.601.10.3.1");
             BigInteger nonce = BigInteger.ValueOf(DateTime.Now.Ticks + Environment.TickCount);
             TimeStampRequest request = tsqGenerator.Generate(DigestAlgorithms.GetAllowedDigests(digestAlgorithm), imprint, nonce);
             byte[] requestBytes = request.GetEncoded();
-            
+
             // Call the communications layer
             respBytes = GetTSAResponse(requestBytes);
-            
+
             // Handle the TSA response
             TimeStampResponse response = new TimeStampResponse(respBytes);
-            
+
             // validate communication level attributes (RFC 3161 PKIStatus)
             response.Validate(request);
             PkiFailureInfo failure = response.GetFailInfo();
             int value = (failure == null) ? 0 : failure.IntValue;
-            if (value != 0) {
+            if (value != 0)
+            {
                 // @todo: Translate value of 15 error codes defined by PKIFailureInfo to string
                 throw new IOException(MessageLocalization.GetComposedMessage("invalid.tsa.1.response.code.2", tsaURL, value));
             }
             // @todo: validate the time stap certificate chain (if we want
             //        assure we do not sign using an invalid timestamp).
-            
+
             // extract just the time stamp token (removes communication status info)
             TimeStampToken tsToken = response.TimeStampToken;
-            if (tsToken == null) {
+            if (tsToken == null)
+            {
                 throw new IOException(MessageLocalization.GetComposedMessage("tsa.1.failed.to.return.time.stamp.token.2", tsaURL, response.GetStatusString()));
             }
             TimeStampTokenInfo tsTokenInfo = tsToken.TimeStampInfo; // to view details
             byte[] encoded = tsToken.GetEncoded();
-            
+
             LOGGER.Info("Timestamp generated: " + tsTokenInfo.GenTime);
-            if (tsaInfo != null) {
+            if (tsaInfo != null)
+            {
                 tsaInfo.InspectTimeStampTokenInfo(tsTokenInfo);
             }
             // Update our token size estimate for the next call (padded to be safe)
             this.tokenSizeEstimate = encoded.Length + 32;
             return encoded;
         }
-        
+
         /**
         * Get timestamp token - communications layer
         * @return - byte[] - TSA response, raw bytes (RFC 3161 encoded)
         */
-        protected internal virtual byte[] GetTSAResponse(byte[] requestBytes) {
+        protected internal virtual byte[] GetTSAResponse(byte[] requestBytes)
+        {
             HttpWebRequest con = (HttpWebRequest)WebRequest.Create(tsaURL);
             con.ContentLength = requestBytes.Length;
             con.ContentType = "application/timestamp-query";
             con.Method = "POST";
-            if ((tsaUsername != null) && !tsaUsername.Equals("") ) {
+            if ((tsaUsername != null) && !tsaUsername.Equals(""))
+            {
                 string authInfo = tsaUsername + ":" + tsaPassword;
                 authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo), Base64FormattingOptions.None);
                 con.Headers["Authorization"] = "Basic " + authInfo;
@@ -229,18 +266,20 @@ namespace iTextSharp.text.pdf.security {
             MemoryStream baos = new MemoryStream();
             byte[] buffer = new byte[1024];
             int bytesRead = 0;
-            while ((bytesRead = inp.Read(buffer, 0, buffer.Length)) > 0) {
+            while ((bytesRead = inp.Read(buffer, 0, buffer.Length)) > 0)
+            {
                 baos.Write(buffer, 0, bytesRead);
             }
             inp.Close();
             response.Close();
             byte[] respBytes = baos.ToArray();
-            
+
             String encoding = response.ContentEncoding;
-            if (encoding != null && Util.EqualsIgnoreCase(encoding, "base64")) {
+            if (encoding != null && Util.EqualsIgnoreCase(encoding, "base64"))
+            {
                 respBytes = Convert.FromBase64String(Encoding.ASCII.GetString(respBytes));
             }
             return respBytes;
-        }    
+        }
     }
 }
