86baafcfc1efacdb20529d502117f096678b0eef
Ilya Idamkin <ilya.idamkin@duallab.com>
2017-04-03 14:29:44 +0300

    Port commit c3e3a452d7: Changed Version to load license key version 2.x.y
    DEVSIX-1143

--- src/core/iTextSharp/text/Version.cs
+++ src/core/iTextSharp/text/Version.cs
@@ -69,13 +69,13 @@ namespace iTextSharp.text {
 	     * iText is a registered trademark by iText Group NV.
 	     * Please don't change this constant.
 	     */
-	    static private String iText = "iTextSharp\u2122";
+	    static private String iText = "iTextSharp\u2122 pdfXfa";
     	
 	    /**
 	     * This String contains the version number of this iText release.
 	     * For debugging purposes, we request you NOT to change this constant.
 	     */
-        static private String release = "5.5.11-SNAPSHOT";
+        static private String release = "1.0.0";
 
 	    /**
 	     * This String contains the iText version as shown in the producer line.
@@ -83,7 +83,7 @@ namespace iTextSharp.text {
 	     * iText Group requests that you retain the iText producer line
 	     * in every PDF that is created or manipulated using iText.
 	     */
-	    private String iTextVersion = iText + " " + release + " \u00a92000-2016 iText Group NV";
+	    private String iTextVersion = iText + " " + release + " \u00a92000-2017 iText Group NV";
 
         /**
          * The license key.
@@ -100,7 +100,7 @@ namespace iTextSharp.text {
                 version = new Version();
                 lock (version) {
                     try {
-                        Type type = Type.GetType("iTextSharp.license.LicenseKey, itextsharp.LicenseKey");
+                        Type type = Type.GetType("iTextSharp.license.XfaLicenseKey, itextsharp.XfaLicenseKey");
                         MethodInfo m = type.GetMethod("GetLicenseeInfo");
                         String[] info = (String[]) m.Invoke(Activator.CreateInstance(type), null);
                         if (info[3] != null && info[3].Trim().Length > 0) {
