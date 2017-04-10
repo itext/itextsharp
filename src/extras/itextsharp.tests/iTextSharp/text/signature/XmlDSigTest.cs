de230aa9f55577f99533317cd5328a17a8a57d15
Alexey Subach <alexey.subach@duallab.com>
2017-04-10 11:14:56 +0300

    Fix XmlDSigTest so that it works for .NET 4.0

--- src/extras/itextsharp.tests/iTextSharp/text/signature/XmlDSigTest.cs
+++ src/extras/itextsharp.tests/iTextSharp/text/signature/XmlDSigTest.cs
@@ -241,10 +241,13 @@ namespace itextsharp.tests.resources.text.signature
         public static bool VerifySignature(String xml) {
             if (!VerifyPackageSignature(xml))
                 Assert.Fail("Internal verification fails");
-            
+
+            // http://stackoverflow.com/questions/13632630/signedxml-checksignature-fails-in-net-4-but-it-works-in-net-3-5-3-or-2
+            CryptoConfig.AddAlgorithm(typeof(MyXmlDsigC14NTransform), "http://www.w3.org/TR/2001/REC-xml-c14n-20010315");
             XmlDocument xmlDocument = new XmlDocument();
             xmlDocument.PreserveWhitespace = true;
             xmlDocument.Load(new XmlTextReader(xml));
+            MyXmlDsigC14NTransform.document = xmlDocument;
             XmlElement signatureElement = (XmlElement)xmlDocument.GetElementsByTagName("Signature")[0];
             SignedXml signedXml = new SignedXml(xmlDocument);
             signedXml.LoadXml(signatureElement);
@@ -355,6 +358,93 @@ namespace itextsharp.tests.resources.text.signature
             foreach(KeyValuePair<string, string> pair in dictLocal)
                 if(!dictExclude.ContainsKey(pair.Key))
                     dest.CreateAttribute("xmlns", pair.Key, "http://www.w3.org/2000/xmlns/", pair.Value);
+        }
+
+        public class MyXmlDsigC14NTransform : XmlDsigC14NTransform
+        {
+            static XmlDocument _document;
+            public static XmlDocument document
+            {
+                set
+                {
+                    _document = value;
+                }
+            }
+
+            public MyXmlDsigC14NTransform() { }
+
+            public override Object GetOutput()
+            {
+                return base.GetOutput();
+            }
+
+            public override void LoadInnerXml(XmlNodeList nodeList)
+            {
+                base.LoadInnerXml(nodeList);
+            }
+
+            protected override XmlNodeList GetInnerXml()
+            {
+                XmlNodeList nodeList = base.GetInnerXml();
+                return nodeList;
+            }
+
+            public XmlElement GetXml()
+            {
+                return base.GetXml();
+            }
+
+            public override void LoadInput(Object obj)
+            {
+                int n;
+                bool fDefaultNS = true;
+
+                XmlElement element = ((XmlDocument)obj).DocumentElement;
+
+                if (element.Name.Contains("SignedInfo"))
+                {
+                    XmlNodeList DigestValue = element.GetElementsByTagName("DigestValue", element.NamespaceURI);
+                    string strHash = DigestValue[0].InnerText;
+                    XmlNodeList nodeList = _document.GetElementsByTagName(element.Name);
+
+                    for (n = 0; n < nodeList.Count; n++)
+                    {
+                        XmlNodeList DigestValue2 = ((XmlElement)nodeList[n]).GetElementsByTagName("DigestValue", ((XmlElement)nodeList[n]).NamespaceURI);
+                        string strHash2 = DigestValue2[0].InnerText;
+                        if (strHash == strHash2) break;
+                    }
+
+                    XmlNode node = nodeList[n];
+
+                    while (node.ParentNode != null)
+                    {
+                        XmlAttributeCollection attrColl = node.ParentNode.Attributes;
+                        if (attrColl != null)
+                        {
+                            for (n = 0; n < attrColl.Count; n++)
+                            {
+                                XmlAttribute attr = attrColl[n];
+                                if (attr.Prefix == "xmlns")
+                                {
+                                    element.SetAttribute(attr.Name, attr.Value);
+                                }
+                                else if (attr.Name == "xmlns")
+                                {
+                                    if (fDefaultNS)
+                                    {
+                                        element.SetAttribute(attr.Name, attr.Value);
+                                        fDefaultNS = false;
+                                    }
+                                }
+                            }
+                        }
+
+                        node = node.ParentNode;
+                    }
+                }
+
+                base.LoadInput(obj);
+            }
         }
     }
 }
