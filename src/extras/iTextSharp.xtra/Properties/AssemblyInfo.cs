2e682c338d79ae5b1b7055f54e5e58704ebaff89
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2019-07-05 11:33:16 +0300

    [RELEASE] 2.0.2-SNAPSHOT -> 2.0.2

--- src/extras/iTextSharp.xtra/Properties/AssemblyInfo.cs
+++ src/extras/iTextSharp.xtra/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.2.0")]
 [assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.0.2")]
--- src/extras/itextsharp.pdfa.tests/Properties/AssemblyInfo.cs
+++ src/extras/itextsharp.pdfa.tests/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.2.0")]
 [assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.0.2")]
