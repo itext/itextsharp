5ef24b21020735fd13ea9bc7dfe6f95d0181c94f
Uladzimir Asipchuk <vladimir.osipchuk@duallab.com>
2020-12-24 02:49:42 +0300

    [RELEASE] 2.0.9-SNAPSHOT -> 2.0.9

--- src/core/AssemblyInfo.cs
+++ src/core/AssemblyInfo.cs
@@ -22,4 +22,4 @@ using Versions.Attributes;
 
 [assembly: AssemblyVersion("2.0.9.0")]
 [assembly: AssemblyFileVersion("2.0.9.0")]
-[assembly: AssemblyInformationalVersion("2.0.9-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.0.9")]
--- src/core/iTextSharp/text/Version.cs
+++ src/core/iTextSharp/text/Version.cs
@@ -78,7 +78,7 @@ namespace iTextSharp.text {
 	     * This String contains the version number of this iText release.
 	     * For debugging purposes, we request you NOT to change this constant.
 	     */
-        private const String release = "2.0.9-SNAPSHOT";
+        private const String release = "2.0.9";
 
 	    /**
 	     * This String contains the iText version as shown in the producer line.
