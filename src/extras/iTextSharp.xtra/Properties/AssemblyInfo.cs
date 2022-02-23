cf0e6ad0b8deae829f6a0a06f657e220c311783f
iText Software <development@itextpdf.com>
2022-02-23 17:39:08 +0300

    [RELEASE] 5.5.14-SNAPSHOT -> 5.5.13.3

--- src/extras/iTextSharp.xtra/Properties/AssemblyInfo.cs
+++ src/extras/iTextSharp.xtra/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("5.5.14")]
-[assembly: AssemblyInformationalVersion("5.5.14-SNAPSHOT")]
+[assembly: AssemblyVersion("5.5.13.3")]
+[assembly: AssemblyInformationalVersion("5.5.13.3")]
