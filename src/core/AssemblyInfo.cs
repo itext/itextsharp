a0d43f39d0310d2b79223dfae3926fc0cbb13790
Yulian Gaponenko <yulian.gaponenko@itextpdf.com>
2022-05-24 23:22:13 +0200

    [RELEASE] 3.0.1-SNAPSHOT -> 3.0.1

--- src/core/AssemblyInfo.cs
+++ src/core/AssemblyInfo.cs
@@ -22,4 +22,4 @@ using Versions.Attributes;
 
 [assembly: AssemblyVersion("3.0.1.0")]
 [assembly: AssemblyFileVersion("3.0.1.0")]
-[assembly: AssemblyInformationalVersion("3.0.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
--- src/extras/itextsharp.pdfa.tests/Properties/AssemblyInfo.cs
+++ src/extras/itextsharp.pdfa.tests/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("3.0.1.0")]
 [assembly: AssemblyFileVersion("3.0.1.0")]
-[assembly: AssemblyInformationalVersion("3.0.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
